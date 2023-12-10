90706bf6cb75befef09f97b20b7e16a0f8dcb5ed
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-10 20:46:19 +0700

    login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -11,9 +11,24 @@ export const register = async (req, res) => {
 
     await newUser.save();
     res.status(200).send("Created successful");
-  } catch (err) {
+  } catch (error) {
     res.status(500).send("Something wrong");
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      usernam: req.body.username,
+    });
+    if (!user) return res.status(404).send("Wrong username or password");
+
+    const isPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!isPassword) return res.status(400).send("Wrong password or username");
+
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (error) {
+    res.status(500).send("Something wrong");
+  }
+};
 export const logout = async (req, res) => {};
