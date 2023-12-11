3b9565baa9a3df4f38f18d11c00cf7c84f6af98b
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-12 00:16:47 +0700

    navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import "./Navbar.scss"
+import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive)
+    };
+  }, [])
+
   return (
-    <div className='navbar'>
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-            <span className='text'>Vfiverr</span>
-            <span className='dot'>.</span>
+          {/* <Link to="/"> */}
+          <span className="text">Vfiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
         </div>
 
         <div className="links">
-            <span>Fiverr Business</span>
-            <span>Explore</span>
-            <span>English</span>
-            <span>Sign in</span>
-            <span>Become a Seller</span>
-            <button>Join</button>
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
         </div>
       </div>
+      {active && 
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
