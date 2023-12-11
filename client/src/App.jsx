6cad66bf8275a7edbb3ec1be18e1f09040ed89eb
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-11 18:49:58 +0700

    nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import React from "react"
+import Navbar from "./components/navbar/Navbar"
+
 function App() {
 
   return (
     <div>
-      Hello, ttv
+      <Navbar />
     </div>
   )
 }
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+            <span className='text'>Vfiverr</span>
+            <span className='dot'>.</span>
+        </div>
+
+        <div className="links">
+            <span>Fiverr Business</span>
+            <span>Explore</span>
+            <span>English</span>
+            <span>Sign in</span>
+            <span>Become a Seller</span>
+            <button>Join</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
