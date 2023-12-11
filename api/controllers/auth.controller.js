3d39a39691751a2ab4991fee7af4bb4a7563b384
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-11 16:38:22 +0700

    logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,4 +46,12 @@ export const login = async (req, res, next) => {
     next(error);
   }
 };
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("Logout");
+};
