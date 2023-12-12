014c87139fd210f09c50a1979268c7efddb452eb
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-12 23:27:24 +0700

    router

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false)
@@ -16,6 +17,12 @@ const Navbar = () => {
       window.removeEventListener("scroll", isActive)
     };
   }, [])
+
+  const currentUser = {
+    id : 1,
+    username:"Vy",
+    isSeller: true
+  }
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -32,8 +39,26 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img src="logo.png" alt="avatar"/>
+              <span>{currentUser?.username}</span>
+              {open && <div className="options">
+                {
+                  currentUser?.isSeller && (
+                    <>
+                    <span>Gigs</span>
+                    <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
       {active && 
