c6d06846e8c6e7824a69089c422090fffc104263
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-13 11:57:50 +0700

    slide

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from '../../components/slide/Slide'
+import { cards } from "../../data"
+import CatCard from '../../components/catCard/CatCard'
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map(card => (
+          <CatCard key={card.id} item={card}/>
+        ))}
+      </Slide>
     </div>
   )
 }
