b53a6562e21e7166b9ee4aeb1a6df499fb074324
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-14 13:11:46 +0700

    chat

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import "./Message.scss"
 
 const Message = () => {
   return (
     <div className='message'>
-      Message Page
+      <div className="container">
+
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link> &gt; Thuy Vy &gt;
+        </span>
+
+        <div className="messages">
+          <div className="item">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func. This is message. I'm writing to test func
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            </p>
+          </div>
+
+          <div className="item">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            </p>
+          </div>
+
+          <div className="item">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img src="/img/person.png" alt="" />
+            <p>This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            This is message. I'm writing to test func
+            </p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="write">
+          <textarea name="" placeholder='message' id="" cols="30" rows="10"></textarea>
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   )
 }
