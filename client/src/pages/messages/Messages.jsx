604c1667c29344f9f548b4f9318209c3c18a1c2e
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-14 11:57:05 +0700

    messages

--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,12 +1,70 @@
-import React from 'react'
-import "./Messages.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Messages.scss";
 
 const Messages = () => {
-  return (
-    <div className='messages'>
-      Messages Page
-    </div>
-  )
-}
+  const currentUser = {
+    id: 1,
+    username: "Vy",
+    isSeller: true,
+  };
 
-export default Messages
+  const message = `Good luck for you`;
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+
+          <tr className="active">
+            <td>Tran Vy</td>
+            <td>
+              <Link to="/message/5" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr className="active">
+            <td>Vy Tran</td>
+            <td>
+              <Link to="/message/5" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>ttv</td>
+            <td>
+              <Link to="/message/5" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
