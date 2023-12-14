0d2e6bd8e8972759587bef9fc382612f7ab11b96
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-14 13:51:20 +0700

    add new

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -4,7 +4,63 @@ import "./Add.scss"
 const Add = () => {
   return (
     <div className='add'>
-      Add New Page
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input type="text" placeholder="I will do something. I'm really good at"/>
+
+            <label htmlFor="">Category</label>
+            <select name='cats' id='cats'>
+              <option value="design">Design</option>
+              <option value="art">Art</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+
+            <label htmlFor="">Cover Image</label>
+            <input type="file"/>
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple/>
+            <label htmlFor="">Descripiton</label>
+            <textarea 
+              name="" 
+              id="" 
+              cols="30" 
+              rows="15"
+              placeholder='Description'
+            >
+            </textarea>
+
+            <button>Create</button>
+          </div>
+
+          <div className="right">
+            <label htmlFor="">Service Title</label>
+            <input type="text" placeholder='One-page web design'/>
+
+            <label htmlFor="">Short Description</label>
+            <textarea name="" id="" cols="30" rows="10" placeholder='Short description'></textarea>
+
+            <label htmlFor="">Delivery Time</label>
+            <input type="number" min={1}/>
+
+            <label htmlFor="">Revision Number</label>
+            <input type="number" min={1}/>
+
+            <label htmlFor="">Add Features</label>
+            <input type="text" placeholder='page design'/>
+            <input type="text" placeholder='file uploading'/>
+            <input type="text" placeholder='setting up a domain'/>
+            <input type="text" placeholder='hosting'/>
+
+            <label htmlFor="">Price</label>
+            <input type="number" min={1} placeholder='Price'/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
