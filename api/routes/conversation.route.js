fb90c4c3ee333e0b5ac88dbcb072765ef2a0842a
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-17 16:05:52 +0700

    end point conversation

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { fn } from "../controllers/conversation.controller.js";
+import {
+  createConversation,
+  updateConversation,
+  getSingleConversation,
+  getConversations,
+} from "../controllers/conversation.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test", fn);
+router.get("/", verifyToken, getConversations);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.post("/", verifyToken, createConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;
